71fd622827ba55e64eeacc1c12654229baa15652
Rosun GV <gvrosun@gmail.com>
2021-06-11 12:59:04 +0530

    Audio and Video setup done

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import sys
 import tempfile
 import sounddevice as sd
 from scipy.io.wavfile import write
+from win32api import GetSystemMetrics
+from pydub import AudioSegment
+from pydub.utils import make_chunks
 
 
 def print_help():
@@ -32,8 +35,33 @@ class Recorder:
     def __init__(self):
         self.args = sys.argv[1:]
         self.video, self.screen, self.mic, self.audio = self.get_details()
-        self.file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        self.file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.mp4'
         self.temp_dir = tempfile.gettempdir()
+
+        # Setting up audio
+        self.freq = 44100
+        self.duration = 600
+        self.recording = sd.rec(int(self.duration * self.freq),
+                                samplerate=self.freq, channels=2)
+
+        # Setting up video
+        self.capture = cv2.VideoCapture(0)
+        width = GetSystemMetrics(0)
+        height = GetSystemMetrics(1)
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        self.captured_video = cv2.VideoWriter(self.file_name, fourcc, 20.0, (width, height))
+        self.start_video()
+
+    def start_video(self):
+        while True:
+            ret, frame = self.capture.read()
+            cv2.imshow('frame', frame)
+            self.captured_video.write(frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.capture.release()
+        cv2.destroyAllWindows()
+        write("recording.wav", self.freq, self.recording)
 
     def get_details(self):
         video = True
@@ -55,8 +83,20 @@ class Recorder:
 
 
 Recorder()
-freq = 44100
-duration = 5
-recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
-sd.wait()
-write("recording.wav", freq, recording)
+
+# from pydub import AudioSegment
+# from pydub.utils import make_chunks
+#
+# ##bluesfile 30s
+# audio = AudioSegment.from_file("blues.00000.wav", "wav")
+#
+#  size = 10000 ##The milliseconds of cutting
+#
+#  chunks = make_chunks(audio, size) ##Cut the file into 10s pieces
+#
+# for i, chunk in enumerate(chunks):
+#          ##Enumeration, i is the index, chunk is the cut file
+#     chunk_name = "bulues{0}.wav".format(i)
+#     print(chunk_name)
+#          ##save document
+#     chunk.export(chunk_name, format="wav")
